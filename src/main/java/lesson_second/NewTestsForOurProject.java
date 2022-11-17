bc934ec4979183a57e855081d97f8dc025d049bc
nikito <aa@aa.aa>
2022-11-17 20:47:29 +0200

    Created new class for test our application

---
+++ src/main/java/lesson_second/NewTestsForOurProject.java
@@ -0,0 +1,19 @@
+package lesson_second;
+
+public class NewTestsForOurProject {
+
+
+    public void test1(){
+        System.out.println("First test");
+    }
+
+    public void test2(){
+        System.out.println("Second test");
+
+    }
+
+    public void test3(){
+        System.out.println("Third test");
+
+    }
+}
