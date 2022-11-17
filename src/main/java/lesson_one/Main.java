50d2e9d0abe87409058650fb23940e59cc2ac950
nikito <aa@aa.aa>
2022-11-17 19:56:00 +0200

    created two additional messages

--- src/main/java/lesson_one/Main.java
+++ src/main/java/lesson_one/Main.java
@@ -3,5 +3,10 @@ package lesson_one;
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello world");
+
+
+        System.out.println("QA Automation the Best");
+        System.out.println("our work with Git");
     }
+
 }
